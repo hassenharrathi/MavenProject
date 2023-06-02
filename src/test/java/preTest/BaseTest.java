65af10023f1d0711b884afdbbf6c0107c9951d40
hassenharrathi <hassenharrathi@yahoo.fr>
2023-06-02 15:44:25 +0100

    Test 1.0-SNAPSHOT

---
+++ src/test/java/preTest/BaseTest.java
@@ -0,0 +1,46 @@
+package preTest;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeAll
+     void setup(){
+        this.driver = new FirefoxDriver();
+        driver.get("https://www.tutorialspoint.com/about/about_careers.htm");
+    }
+
+    @AfterAll
+    void tearDown() {
+        // Quit the browser
+        if (driver != null) {
+            driver.quit();
+        }
+
+        // Take a screenshot in case of test failure
+        if (driver instanceof TakesScreenshot) {
+            TakesScreenshot screenshotDriver = (TakesScreenshot) driver;
+            try {
+                // Capture the screenshot as a file
+                Path screenshotPath = Paths.get("path/to/screenshot.png");
+                Files.copy(screenshotDriver.getScreenshotAs(OutputType.FILE).toPath(), screenshotPath);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/test/java/test/login/LoginTest.java
@@ -0,0 +1,46 @@
+package test.login;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import pages.AcceuilPage;
+import pages.LoginPage;
+import preTest.BaseTest;
+import java.time.Duration;
+
+public class LoginTest extends BaseTest {
+    @Test
+    void testLogin ()  {
+
+        // Déclaration attributs
+        String username = "hassenharrathi@yahoo.fr";
+        String password = "Ch*1982*";
+
+        // Temps d'attente
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(100));
+
+        // Instanciation
+        LoginPage loginPage = new LoginPage(driver);
+        AcceuilPage acceuilPage = new AcceuilPage(driver);
+
+        // Attente explicite pour l'affichage du panneau de connexion
+        //wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@href='https://www.tutorialspoint.com/market/login.asp']")));
+        wait.until(ExpectedConditions.elementToBeClickable(acceuilPage.getConnectionPanel())).click();
+
+        // Saisie des identifiants de connexion
+        loginPage.getLoginEmailPanel().sendKeys(username);
+        loginPage.getLoginPswPanel().sendKeys(password);
+        loginPage.getSubmitPanel().click();
+
+        // Attente explicite pour la fermeture de la fenêtre contextuelle
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[text()='Skip']")));
+        wait.until(ExpectedConditions.elementToBeClickable(loginPage.getPopUpPanel())).click();
+
+        // Déconnexion
+        acceuilPage.getLogOut().click();
+        acceuilPage.getSignOut().click();
+
+    }
+
+}
